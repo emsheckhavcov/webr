ce1cd681c309cde9e7fb64c4c5a09f56491fb2e2
emsheckhavcov <deadwarior04@gmail.com>
2026-03-14 11:38:20 +0300

    Created react app with formulas

---
+++ src/components/pythagor.tsx
@@ -0,0 +1,13 @@
+export function Pythagor() {
+  return (
+    <div>
+      <p>1. E = mc<sup>2</sup></p>
+      <p>2. a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup></p>
+      <p>3. V = πr<sup>2</sup>h</p>
+      <p>4. y = ax<sup>2</sup> + bx + c</p>
+      <p>5. (a + b)<sup>2</sup> = a<sup>2</sup> + 2ab + b<sup>2</sup></p>
+      <p>6. H<sub>2</sub>O</p>
+      <p>7. log<sub>b</sub> a</p>
+    </div>
+  );
+}
